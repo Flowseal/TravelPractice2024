b5caa6e0d98a891c3ccb4243e00891c4f35d067d
Артём Вещев <sellyalegium@gmail.com>
2024-07-17 18:47:46 +0300

    Author DateTime -> DateOnly

--- Theater/Domain/Models/Author.cs
+++ Theater/Domain/Models/Author.cs
@@ -4,11 +4,11 @@ public class Author
 {
     public int Id { get; private init; }
     public string Name { get; private init; }
-    public DateTime BirthDate { get; private init; }
+    public DateOnly BirthDate { get; private init; }
 
     public List<Composition> Compositions { get; private init; } = new List<Composition>();
 
-    public Author( string name, DateTime birthDate )
+    public Author( string name, DateOnly birthDate )
     {
         Name = name;
         BirthDate = birthDate;
